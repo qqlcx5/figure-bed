6914244c215c24de48d0aa0efe272dd8c65e881e
qqlcx5 <qqlcx5@163.com>
2022-01-14 09:44:54 +0800

    tags

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ git commit -m "tags"
 git push origin master
 git tag -d basics
 # 打标签
-git tag -a basics -m "image"
+
+date=`date +%Y-%m-%d_%H:%M:%S`
+git tag -a basics -m "deploy ${date}"
 git push origin --tags -f
 cd -
